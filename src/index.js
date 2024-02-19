c730c44bd72e13014e0853a3382a207371f2d3b9
김동영/서비스플랫폼개발팀/EP <dongyoung.kim@nhnenter.com>
2024-02-19 16:06:13 +0900

    fix: base url 변경 테스트

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-axios.defaults.baseURL = "http://app-df728c8dca4cb8e0ad95f0bc709db75f-chart.50010-fccb3e.svc.cluster.local"
+axios.defaults.baseURL = "https://1d23a3054347fa342b6e7d53bd3d1aef.apppaas.io"
 root.render(
   <React.StrictMode>
     <App />
